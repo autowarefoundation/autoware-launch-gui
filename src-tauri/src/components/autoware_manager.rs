f3eeb81eba538822a89cee02fa8dd8740a0f67af
KhalilSelyan <khalilselyan2@gmail.com>
2024-01-29 12:44:20 +0300

    feat: get services

--- src-tauri/src/components/autoware_manager.rs
+++ src-tauri/src/components/autoware_manager.rs
@@ -226,3 +226,33 @@ pub async fn get_topics() -> Result<Vec<String>, String> {
     }
     Ok(topics)
 }
+
+#[tauri::command]
+pub async fn get_services() -> Result<Vec<String>, String> {
+    let mut services = Vec::new();
+
+    let cmd_str = format!(
+        "source /opt/ros/humble/setup.bash && \
+         ros2 service list -t"
+    );
+
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(&cmd_str)
+        .output()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let lines = stdout.split("\n");
+    for line in lines {
+        if line != "" {
+            let service = line.split(" ").collect::<Vec<&str>>()[0];
+            let type_ = line.split(" ").collect::<Vec<&str>>()[1];
+            let type_ = type_.trim_start_matches('[').trim_end_matches(']');
+
+            services.push(format!("{} {}", service, type_));
+        }
+    }
+    Ok(services)
+}
